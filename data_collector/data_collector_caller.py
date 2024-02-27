c38693f57d20fa5bb9ff5b4520dcf7ed97586d07
vzjp <23609056+vzjp@users.noreply.github.com>
2024-02-27 13:05:52 +0900

    enable change date using system args

--- data_collector/data_collector_caller.py
+++ data_collector/data_collector_caller.py
@@ -17,43 +17,49 @@ ana_collection_list = [
     ['SPK', ['WKJ', 'MMB', 'KUH', 'HKD', 'AOJ', 'AXT', 'SDJ', 'KIJ', 'TOY', 'KMQ', 'HIJ', 'FUK']]
 ]
 
-def scrape_jal():
+def scrape_jal(date="prev"):
     collector = data_collector.JalScraper()
     for collection in jal_collection_list:
         for to in collection[1]:
             # 往路
             collector.set_from(collection[0])
             collector.set_to(to)
-            collector.set_date("prev")
+            collector.set_date(date)
             collector.scrape("jal.csv")
             time.sleep(3)
             # 復路
             collector.set_from(to)
             collector.set_to(collection[0])
-            collector.set_date("prev")
+            collector.set_date(date)
             collector.scrape("jal.csv")
             time.sleep(3)
 
-def scrape_ana():
+def scrape_ana(date="prev"):
     collector = data_collector.AnaScraper()
     for collection in ana_collection_list:
         for to in collection[1]:
             # 往路
             collector.set_from(collection[0])
             collector.set_to(to)
-            collector.set_date("prev")
+            collector.set_date(date)
             collector.scrape("ana.csv")
             time.sleep(3)
             # 復路
             collector.set_from(to)
             collector.set_to(collection[0])
-            collector.set_date("prev")
+            collector.set_date(date)
             collector.scrape("ana.csv")
             time.sleep(3)
 
+import sys
 
-thread_jal = threading.Thread(target=scrape_jal)
-thread_ana = threading.Thread(target=scrape_ana)
+if len(sys.argv) > 1:
+    date = sys.argv[1]
+    thread_jal = threading.Thread(target=scrape_jal, args=(date,))
+    thread_ana = threading.Thread(target=scrape_ana, args=(date,))
+else:
+    thread_jal = threading.Thread(target=scrape_jal)
+    thread_ana = threading.Thread(target=scrape_ana)
 
 thread_jal.start()
 thread_ana.start()
