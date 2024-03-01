fcd2a0969085cc10389010f0a5a11b3be5bb7a61
vzjp <23609056+vzjp@users.noreply.github.com>
2024-03-01 15:08:04 +0900

    add sufix for the csv file

--- data_collector/data_collector_caller.py
+++ data_collector/data_collector_caller.py
@@ -1,6 +1,7 @@
 import data_collector
 import time
 import threading
+import datetime
 
 jal_collection_list = [
     ['NRT', ['CTS', 'NGO', 'ITM']],
@@ -17,7 +18,7 @@ ana_collection_list = [
     ['SPK', ['WKJ', 'MMB', 'KUH', 'HKD', 'AOJ', 'AXT', 'SDJ', 'KIJ', 'TOY', 'KMQ', 'HIJ', 'FUK']]
 ]
 
-def scrape_jal(date="prev"):
+def scrape_jal(date="prev", sufix = ""):
     collector = data_collector.JalScraper()
     for collection in jal_collection_list:
         for to in collection[1]:
@@ -25,16 +26,16 @@ def scrape_jal(date="prev"):
             collector.set_from(collection[0])
             collector.set_to(to)
             collector.set_date(date)
-            collector.scrape("jal.csv")
+            collector.scrape(f"jal{sufix}.csv")
             time.sleep(3)
             # 復路
             collector.set_from(to)
             collector.set_to(collection[0])
             collector.set_date(date)
-            collector.scrape("jal.csv")
+            collector.scrape(f"jal{sufix}.csv")
             time.sleep(3)
 
-def scrape_ana(date="prev"):
+def scrape_ana(date="prev", sufix=""):
     collector = data_collector.AnaScraper()
     for collection in ana_collection_list:
         for to in collection[1]:
@@ -42,24 +43,34 @@ def scrape_ana(date="prev"):
             collector.set_from(collection[0])
             collector.set_to(to)
             collector.set_date(date)
-            collector.scrape("ana.csv")
+            collector.scrape(f"ana{sufix}.csv")
             time.sleep(3)
             # 復路
             collector.set_from(to)
             collector.set_to(collection[0])
             collector.set_date(date)
-            collector.scrape("ana.csv")
+            collector.scrape(f"ana{sufix}.csv")
             time.sleep(3)
 
 import sys
 
 if len(sys.argv) > 1:
     date = sys.argv[1]
-    thread_jal = threading.Thread(target=scrape_jal, args=(date,))
-    thread_ana = threading.Thread(target=scrape_ana, args=(date,))
+    date_str = ""
+    if date == "prev":
+        date_str = (datetime.date.today() - datetime.timedelta(days=1)).strftime('%Y%m%d')
+    elif date == "today":
+        date_str = datetime.date.today().strftime('%Y%m%d')
+    elif date == "next":
+        date_str = (datetime.date.today() + datetime.timedelta(days=1)).strftime('%Y%m%d')
+    thread_jal = threading.Thread(target=scrape_jal, args=(date,date_str))
+    thread_ana = threading.Thread(target=scrape_ana, args=(date,date_str))
 else:
-    thread_jal = threading.Thread(target=scrape_jal)
-    thread_ana = threading.Thread(target=scrape_ana)
+    # this will be called when no sys args placed.
+    # this means that the target to get is "prev"
+    date_str = (datetime.date.today() - datetime.timedelta(days=1)).strftime('%Y%m%d')
+    thread_jal = threading.Thread(target=scrape_jal, args=("prev", date_str))
+    thread_ana = threading.Thread(target=scrape_ana, args=("prev", date_str))
 
 thread_jal.start()
 thread_ana.start()
