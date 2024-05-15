1f9d2b9cf7e5780b49a3a9946c4add9416383940
vzjp <23609056+vzjp@users.noreply.github.com>
2024-05-15 23:17:59 +0900

    [fix] bug which happen when dir already exists fix

--- data_collector/data_collector_caller.py
+++ data_collector/data_collector_caller.py
@@ -83,7 +83,8 @@ def move_to_data_dir(filename:str):
         return
     icaocode = filename[:3]
     path = os.path.join(os.path.dirname(os.path.abspath(__file__)), icaocode, 'analyze_target', date_str)
-    os.makedirs(path)
+    if not os.path.exists(path):
+        os.makedirs(path)
     shutil.move(filename, os.path.join(path, filename))
 
 def first_last_day_of_week(date:datetime.datetime) -> tuple[datetime.datetime, datetime.datetime]:
