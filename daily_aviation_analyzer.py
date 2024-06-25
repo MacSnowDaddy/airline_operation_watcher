9560475fda6b5cde994fb65cf9417798d4889b90
vzjp <23609056+vzjp@users.noreply.github.com>
2024-06-25 23:46:45 +0900

    [fix] fit daily_aviation_analyzer to 3cbdc934

--- daily_aviation_analyzer.py
+++ daily_aviation_analyzer.py
@@ -5,8 +5,8 @@ def main():
     data_collector_caller.main()
     import datetime
     yesterday = datetime.date.today() - datetime.timedelta(days=1)
-    会社別RJTT2RJCC定時到着率.main(yesterday, "HND", "CTS")
-    会社別RJTT2RJCC定時到着率.main(yesterday, "CTS", "HND")
+    会社別RJTT2RJCC定時到着率.main(yesterday.year, yesterday.month, yesterday.day, "HND", "CTS")
+    会社別RJTT2RJCC定時到着率.main(yesterday.year, yesterday.month, yesterday.day, "CTS", "HND")
     
 
 
