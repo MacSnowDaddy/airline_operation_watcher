9578aecdb89fcee157bcf106a35dfb2361fd0524
vzjp <23609056+vzjp@users.noreply.github.com>
2024-07-23 13:50:18 +0900

    [fix] error because position of import

--- daily_aviation_analyzer.py
+++ daily_aviation_analyzer.py
@@ -12,7 +12,6 @@ def main():
     start_time = datetime.datetime.now()
 
     data_collector_caller.main()
-    import datetime
     yesterday = datetime.date.today() - datetime.timedelta(days=1)
     会社別RJTT2RJCC定時到着率.main(yesterday.year, yesterday.month, yesterday.day, "HND", "CTS")
     会社別RJTT2RJCC定時到着率.main(yesterday.year, yesterday.month, yesterday.day, "CTS", "HND")
