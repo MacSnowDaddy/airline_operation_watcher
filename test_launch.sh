e8e37863f0b437a6f690815ece3bd7bc5f45803b
MacSnowDaddy <macsnowdaddy@gmail.com>
2024-08-04 23:40:35 +0900

    [fix] Add PYTHONPATH to launch scripts
    This commit adds the PYTHONPATH to the launch scripts to ensure that the
    correct version of the Python interpreter is used. This is necessary
    because the Python interpreter is installed in a virtual environment.

--- test_launch.sh
+++ test_launch.sh
@@ -2,4 +2,5 @@ export TARGET="test"
 export ENV="ec2"
 Xvfb :99 -screen 0 1280x720x16 &
 export DISPLAY=:99
-python3 daily_aviation_analyzer.py
+export PYTHONPATH=/home/ec2-user/.pyenv/versions/3.10.4/lib/python3.10/site-packages:$PYTHONPATH
+/home/ec2-user/.pyenv/shims/python3 daily_aviation_analyzer.py >> /home/ec2-user/test_launch.log 2>&1
