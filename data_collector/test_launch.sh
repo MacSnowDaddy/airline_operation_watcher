412afdc3381fbfdf469ad6e4b7e38c1ad0f59163
MacSnowDaddy <macsnowdaddy@gmail.com>
2024-08-06 09:26:31 +0900

    [fix]Add time zone setting

--- data_collector/test_launch.sh
+++ data_collector/test_launch.sh
@@ -2,6 +2,7 @@ export TARGET="test"
 export ENV="ec2"
 Xvfb :99 -screen 0 1280x720x16 &
 export DISPLAY=:99
+sudo timedatectl set-timezone Asia/Tokyo
 export PYTHONPATH=/home/ec2-user/.pyenv/versions/3.10.4/lib/python3.10/site-packages:$PYTHONPATH
 cd /home/ec2-user/airline-ops/airline_operation_watcher/data_collector
 /home/ec2-user/.pyenv/shims/python3 data_collector_caller.py ana prev >> /home/ec2-user/production_launch.log 2>&1 &
