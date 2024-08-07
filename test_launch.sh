2bf9a8b664e0920a0fc683895df44ad40098f3d5
MacSnowDaddy <macsnowdaddy@gmail.com>
2024-08-08 05:38:37 +0900

    [fix]correct test_launch.sh to latest version(as prod_launch.sh)

--- test_launch.sh
+++
@@ -1,7 +0,0 @@
-export TARGET="test"
-export ENV="ec2"
-touch /home/ec2-user/test_launch_called.log
-Xvfb :99 -screen 0 1280x720x16 &
-export DISPLAY=:99
-export PYTHONPATH=/home/ec2-user/.pyenv/versions/3.10.4/lib/python3.10/site-packages:$PYTHONPATH
-/home/ec2-user/.pyenv/shims/python3 daily_aviation_analyzer.py >> /home/ec2-user/test_launch.log 2>&1
