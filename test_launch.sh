7f2a0e3409dca3637372d237845ef2e5d5ae67d5
MacSnowDaddy <macsnowdaddy@gmail.com>
2024-08-12 13:19:45 +0900

    [fix]Call data_collector_caller.py instead to call daily_aviation_analyzer.py
    On EC2, it appears that selenium chrome will crash when I use threading in my
    python script. So I call data_collector_caller.py instead of calling
    daily_aviation_analyzer.py

--- test_launch.sh
+++ test_launch.sh
@@ -3,4 +3,7 @@ export ENV="ec2"
 Xvfb :99 -screen 0 1280x720x16 &
 export DISPLAY=:99
 export PYTHONPATH=/home/ec2-user/.pyenv/versions/3.10.4/lib/python3.10/site-packages:$PYTHONPATH
-/home/ec2-user/.pyenv/shims/python3 daily_aviation_analyzer.py >> /home/ec2-user/test_launch.log 2>&1
+/home/ec2-user/.pyenv/shims/python3 data_collector_caller.py ana prev >> /home/ec2-user/production_launch.log 2>&1 &
+/home/ec2-user/.pyenv/shims/python3 data_collector_caller.py jal prev >> /home/ec2-user/production_launch.log 2>&1 &
+/home/ec2-user/.pyenv/shims/python3 data_collector_caller.py sky prev >> /home/ec2-user/production_launch.log 2>&1 &
+/home/ec2-user/.pyenv/shims/python3 data_collector_caller.py ado prev >> /home/ec2-user/production_launch.log 2>&1 &
