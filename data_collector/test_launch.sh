18ad945930071f029ec7bbaf3e40ce22ccf44fc7
MacSnowDaddy <macsnowdaddy@gmail.com>
2024-08-12 12:40:49 +0900

    [refact]move dir of prod_launch.sh

--- data_collector/test_launch.sh
+++ data_collector/test_launch.sh
@@ -1,8 +1,9 @@
-export TARGET="production"
+export TARGET="test"
 export ENV="ec2"
 Xvfb :99 -screen 0 1280x720x16 &
 export DISPLAY=:99
 export PYTHONPATH=/home/ec2-user/.pyenv/versions/3.10.4/lib/python3.10/site-packages:$PYTHONPATH
+cd /home/ec2-user/airline-ops/airline_operation_watcher/data_collector
 /home/ec2-user/.pyenv/shims/python3 data_collector_caller.py ana prev >> /home/ec2-user/production_launch.log 2>&1 &
 /home/ec2-user/.pyenv/shims/python3 data_collector_caller.py jal prev >> /home/ec2-user/production_launch.log 2>&1 &
 /home/ec2-user/.pyenv/shims/python3 data_collector_caller.py sky prev >> /home/ec2-user/production_launch.log 2>&1 &
