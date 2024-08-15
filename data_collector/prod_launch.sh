45bca01ae5af201684cba6d17d193c24a672579c
MacSnowDaddy <macsnowdaddy@gmail.com>
2024-08-15 23:11:56 +0900

    [fix]Change running directory to fix import error
    Import error occers concering data_collector/ana_analyzer and so.

--- data_collector/prod_launch.sh
+++ data_collector/prod_launch.sh
@@ -4,14 +4,14 @@ Xvfb :99 -screen 0 1280x720x16 &
 export DISPLAY=:99
 sudo timedatectl set-timezone Asia/Tokyo
 export PYTHONPATH=/home/ec2-user/.pyenv/versions/3.10.4/lib/python3.10/site-packages:$PYTHONPATH
-cd /home/ec2-user/airline-ops/airline_operation_watcher/data_collector
-/home/ec2-user/.pyenv/shims/python3 data_collector_caller.py ana prev >> /home/ec2-user/production_launch.log 2>&1 &
+cd /home/ec2-user/airline-ops/airline_operation_watcher
+/home/ec2-user/.pyenv/shims/python3 data_collector/data_collector_caller.py ana prev >> /home/ec2-user/production_launch.log 2>&1 &
 PID_ANA=$!
-/home/ec2-user/.pyenv/shims/python3 data_collector_caller.py jal prev >> /home/ec2-user/production_launch.log 2>&1 &
+/home/ec2-user/.pyenv/shims/python3 data_collector/data_collector_caller.py jal prev >> /home/ec2-user/production_launch.log 2>&1 &
 PID_JAL=$!
-/home/ec2-user/.pyenv/shims/python3 data_collector_caller.py sky prev >> /home/ec2-user/production_launch.log 2>&1 &
+/home/ec2-user/.pyenv/shims/python3 data_collector/data_collector_caller.py sky prev >> /home/ec2-user/production_launch.log 2>&1 &
 PID_SKY=$!
-/home/ec2-user/.pyenv/shims/python3 data_collector_caller.py ado prev >> /home/ec2-user/production_launch.log 2>&1 &
+/home/ec2-user/.pyenv/shims/python3 data_collector/data_collector_caller.py ado prev >> /home/ec2-user/production_launch.log 2>&1 &
 PID_ADO=$!
 
 wait $PID_ANA
