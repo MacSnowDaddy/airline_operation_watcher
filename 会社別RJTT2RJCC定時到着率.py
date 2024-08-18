e05278aa46b7997b1ef622c8d0c884879422ce91
MacSnowDaddy <macsnowdaddy@gmail.com>
2024-08-18 13:05:10 +0900

    [feat]Shorten output of on-time rate
    To make short of output in order to post X, replace "(" to " "(white space).

--- 会社別RJTT2RJCC定時到着率.py
+++ 会社別RJTT2RJCC定時到着率.py
@@ -38,7 +38,7 @@ def print_on_schedule_arrival_rate(airline:str, df:pd.DataFrame, dep_ap:str='*',
         df = df[(df['from'] == ap_dict.decode(dep_ap,  airline))]
     if dest_ap != "*":
         df = df[(df['to']   == ap_dict.decode(dest_ap, airline))]
-    print(f'{airline.upper()} {on_schedule_arrival_rate(df)*100:.1f}% ({len(df[df["arr_delay"] <= 14])}/{len(df)})')
+    print(f'{airline.upper()} {on_schedule_arrival_rate(df)*100:.1f}% {len(df[df["arr_delay"] <= 14])}/{len(df)}')
     df['hour'] = df['schedule_arr'].str.split(':').str[0]
     group_hour = df.groupby('hour')['arr_delay'].describe()
     group_hour.index = group_hour.index.astype(int)
