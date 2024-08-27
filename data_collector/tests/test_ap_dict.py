23977bb0b327b19ce3415038089aa44c1fcda65e
MacSnowDaddy <macsnowdaddy@gmail.com>
2024-08-27 09:35:26 +0900

    [test bug]Fix ap_dict test bug

--- data_collector/tests/test_ap_dict.py
+++ data_collector/tests/test_ap_dict.py
@@ -9,11 +9,11 @@ class TestApDict(unittest.TestCase):
         pass
 
     def test_decode(self):
-        self.assertEqual(ap_dict.decode("HND"), "東京(羽田)")
+        self.assertEqual(ap_dict.decode("HND"), "東京（羽田）")
         self.assertEqual(ap_dict.decode("WKJ", company="ana"), "稚内")
     
     def test_encode(self):
-        self.assertEqual(ap_dict.encode("東京(羽田)"), "HND")
+        self.assertEqual(ap_dict.encode("東京（羽田）"), "HND")
         self.assertEqual(ap_dict.encode("稚内", company="ana"), "WKJ")
 
 if __name__ == "__main__":
